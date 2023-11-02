d2da7e17827b040e4b853e1fec60809aa24ad369
Maria <zeldacamila0913@gmail.com>
2023-11-01 19:19:08 -0500

    Answered question 5 of part 1 of the first quiz project. Part 1 completed. Corresponding tests passed.

--- part1/question5.py
+++ part1/question5.py
@@ -20,7 +20,11 @@
 
 sql_create_favorite_foods = """
 
-Your SQL here.
+CREATE TABLE favorite_foods (
+  food_id integer,
+  name text not null,
+  vegetarian integer not null 
+)
 
 """
 
@@ -30,7 +34,8 @@ Your SQL here.
 
 sql_alter_tables_with_favorite_food = """
 
-Your SQL here.
+ALTER TABLE animals ADD COLUMN favorite_food_id integer;
+ALTER TABLE people ADD COLUMN favorite_food_id integer;
 
 """
 
@@ -40,6 +45,9 @@ Your SQL here.
 
 sql_select_all_vegetarian_pets = """
 
-Your SQL here.
+SELECT a.name, ff.name
+FROM animals AS a
+JOIN favorite_foods AS ff ON a.favorite_food_id = ff.food_id
+WHERE ff.vegetarian = 1
 
 """
